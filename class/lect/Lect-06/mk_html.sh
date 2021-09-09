e866ed4f10aa21ce675c165d841d71122986c9d4
Philip Schlump <pschlump@gmail.com>
2021-09-09 06:47:58 -0600

    Save intermediate during Make

---
+++ class/lect/Lect-06/mk_html.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR=../../../
+
+# echo "1=$1 2=$2"
+bn=$(basename $1 .raw.md)
+# echo "bn=$bn"
+
+m4 -P $bn.raw.md >$bn.tmp
+blackfriday-tool ./$bn.tmp > $bn.tmp2
+cat ./${DIR}/css/pre ./${DIR}/css/markdown.css ./${DIR}/css/post ./${DIR}/css/md.css ./${DIR}/css/hpre $bn.tmp2 ./${DIR}/css/hpost >$bn.md
+echo "file://$(pwd)/$bn.html" >>open.1
+rm -f $bn.tmp $bn.tmp2
